048988d670c19abe07212e21797453b04b20950d
Victor <victor.239020@fmm.org.br>
2025-05-01 13:48:04 -0400

    bora ia

--- config/openai.js
+++ config/openai.js
@@ -1,6 +1,8 @@
 import OpenAI from 'openai';
 import dotenv from 'dotenv';
 dotenv.config();
+console.log("Chave OpenAI:", process.env.OPENAI_API_KEY ? "carregada" : "NÃO carregada");
+
 
 const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
